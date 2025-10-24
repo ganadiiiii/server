d7eb2cb26a88af7bc13dcb01b9dadadc0c86fc6a
Jonghoon Hwang <hoonly01@naver.com>
2025-10-24 20:52:37 +0900

    fix: update delete method in FriendshipsRepository with custom query

--- src/main/java/ganadii/hanjum/repository/FriendshipsRepository.java
+++ src/main/java/ganadii/hanjum/repository/FriendshipsRepository.java
@@ -2,6 +2,9 @@ package ganadii.hanjum.repository;
 
 import ganadii.hanjum.domain.Friendships;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +16,10 @@ public interface FriendshipsRepository extends JpaRepository<Friendships, Long> 
     List<Friendships> findByUser_UserId(UUID userId);
     List<Friendships> findByFriend_UserId(UUID friendId);
     boolean existsByUser_UserIdAndFriend_UserId(UUID userId, UUID friendId);
-    void deleteByUser_UserIdAndFriend_UserId(UUID userId, UUID friendId);
+
+    @Modifying
+    @Query("DELETE FROM Friendships f WHERE f.user.userId = :userId AND f.friend.userId = :friendId")
+    void deleteByUser_UserIdAndFriend_UserId(@Param("userId") UUID userId, @Param("friendId") UUID friendId);
+
     long countByUser_UserId(UUID userId);
 }
